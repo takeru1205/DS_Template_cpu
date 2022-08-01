a5db2cc44a4c5710e767cb030e155ab95e236961
Takeru Matsumoto <takeru0639418@gmail.com>
2022-08-01 10:31:19 +0900

    Initial commit

---
+++ load_data.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+from logging import getLogger
+
+TRAIN_DATA = 'data/train.csv'
+TEST_DATA = 'data/test.csv'
+
+logger = getLogger(__name__)
+
+
+def read_csv(path):
+    logger.debug('enter')
+    df = pd.read_csv(path)
+    logger.debug('exit')
+    return df
+
+
+def load_train_data():
+    logger.debug('enter')
+    df = read_csv(TRAIN_DATA)
+    logger.debug('exit')
+    return df
+
+
+def load_test_data():
+    logger.debug('enter')
+    df = read_csv(TEST_DATA)
+    logger.debug('exit')
+    return df
+
+
+if __name__ == '__main__':
+    print(load_train_data().head())
+    print(load_test_data().head())
+
+
